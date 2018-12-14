35061cea2f5f5d788cca933248bfc0dfb4847373
Shubham Kandharkar <shubhamkandharkar@gmail.com>
2018-12-14 15:50:07 +0530

    Update Consumer.java
    added System.out.println

--- Consumer.java
+++ Consumer.java
@@ -3,6 +3,6 @@ package com.dependency;
 public interface Consumer {
 	
 	void processMessages(String msg, String rec);
-	System.out.println("Hello World");
+	System.out.println("Hello World!!!);
 
 }
